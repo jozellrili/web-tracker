0e2135fed3623d5a6484a079d0af92cb5975022b
Jerome <switch@redmorph.com>
2015-05-09 18:02:07 +0800

    complete fetching web content without category

--- index.php
+++ index.php
@@ -1,33 +1,51 @@
 <?php
-echo "Welcome to Web Crawler!\n";
-
 
 $to_crawl = "http://animefreak.tv";
-
-function get_links($url) {
-	$input = @file_get_contents($url);
-	$regexp = "<a\s[^>]*href=(\"??)([^\" >]*?)\\1[^>]*>(.*)<\/a>";
-	preg_match_all("/$regexp/siU",$input, $matches);
 	
+function get_links($url) {
+	$input  = @file_get_contents($url);
+	$regexp = "<a\s[^>]*href=(\"??)([^\">]*?)\\1[^>]*>(.*)<\/a>";
+	preg_match_all("/$regexp/siU", $input, $matches);
+	$base_url = parse_url($url, PHP_URL_HOST);
 	$l = $matches[2];
 	
 	foreach($l as $link) {
 		
-		
-		
 		if (strpos($link, "#")) {
-			$link = substr($link, 0 , strpos($link, "#"));
+			$link = substr($link, 0, strpos($link, "#"));
 		}
-		 
 		if (substr($link,0,1) == ".") {
 			$link = substr($link, 1);
 		}
+		if (substr($link,0,7) == "http://") {
+			$link = $link;
+		}
+		else if (substr($link,0,8) == "https://") {
+			$link = $link;
+		}
+		else if (substr($link,0,2) == "//") {
+			$link = substr($link, 2);
+		}
+		else if (substr($link,0,1) == "#") {
+			$link = $url;
+		}
+		else if (substr($link,0,7) == "mailto:") {
+			$link = "[".$link."]";
+		}
+		else {
+			if (substr($link, 0, 1) != "/") {
+				$link = $base_url."/".$link;
+			}
+			else {
+				$link = $base_url.$link;
+			}
+		}
 		
-		echo $link."<br />";
+		echo $link."<br/>";
 
 	}
 }
-
+	
 get_links($to_crawl);
 
-?>
+
