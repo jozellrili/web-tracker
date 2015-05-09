5e9d082d09f2e332c0f3f106fbe587be7241d998
Jozell M. Rili <sati@redmorph.com>
2015-05-09 15:37:45 +0800

    change array display

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <?php
-$to_crawl = "http://bestspace.co";
+echo "Welcome to Web Crawler!\n";
+
+
+$to_crawl = "http://animefreak.tv";
+
 function get_links($url) {
 	$input = @file_get_contents($url);
-	echo $input;
+	$regexp = "<a\s[^>]*href=(\"??)([^\" >]*?)\\1[^>]*>(.*)<\/a>";
+	preg_match_all("/$regexp/siU",$input, $matches);
+	
+	/* echo "<pre>";
+	print_r($matches[2]);
+	echo "</pre>"; 
+	*/
+	
+	$l = $matches[2];
+	
+	foreach($l as $link) {
+		echo $link."<br />";
+	}
 }
+
 get_links($to_crawl);
-
-<<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-
-</body>
-</html>
 
 ?>
