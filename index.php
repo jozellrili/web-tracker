efdc7f9bc1ebfcee45251eb1d672693c094289a6
Jerome Ruiz <switch@redmorph.com>
2015-09-29 14:42:47 +0800

    add include header file

--- index.php
+++ index.php
@@ -1,29 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<!-- Header -->
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Redmorph</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link rel="icon" type="image/png" href="img/logo1.png" size ="16x16">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/web_crawler.css" rel="stylesheet">
-
-
-    <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
-
-   
-</head>
+<?php
+include("header.php");
+?>
 
 <body id='form'>
    <!-- Navigation -->
